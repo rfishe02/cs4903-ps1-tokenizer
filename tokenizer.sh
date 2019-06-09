8f4253a09bdc9f38c2e0e98ca497e6c6c98adb92
rfishe02 <rfishe02@g.uafs.edu>
2019-06-09 13:05:51 -0500

    More fixes

--- tokenizer.sh
+++ tokenizer.sh
@@ -11,5 +11,7 @@ cat $2/*.out > ./tokens.out
 
 tr -sc 'A-Za-z0-9' '\n' < tokens.out | tr A-Z a-z | sort | uniq -c | sort -n -r > frequency.txt
 
-rm -rf *.java
-rm -rf *.class
+rm *.java
+rm *.class
+rm -rf ./output
+rm tokens.out
