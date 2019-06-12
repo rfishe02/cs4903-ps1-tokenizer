0aa1e127bedd5034d8206e3204f7f0250acd01a0
rfishe02 <rfishe02@g.uafs.edu>
2019-06-12 17:48:20 -0500

    Made changes for linux

--- tokenizer.sh
+++ tokenizer.sh
@@ -7,7 +7,7 @@ javac ./*.java
 
 time java UATokenizer $1 $2
 
-cat $2/*.out > ./tokens.out
+cat $2/*.txt > ./tokens.out
 
 tr -sc 'A-Za-z0-9' '\n' < tokens.out | tr A-Z a-z | sort | uniq -c | sort -n -r > frequency.txt
 
