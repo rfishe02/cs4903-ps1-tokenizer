417020f47d80961395af99c6a5e6389c45cb7a1f
rfishe02 <rfishe02@g.uafs.edu>
2019-06-14 07:28:20 -0500

    made changes to script

--- tokenizer.sh
+++ tokenizer.sh
@@ -16,9 +16,16 @@ cat $2/*.txt > ./tokens.out
 
 tr -sc 'A-Za-z0-9' '\n' < tokens.out | tr A-Z a-z | sort | uniq -c | sort -n -r > frequency.txt
 
-rm *.java
-rm *.class
-rm tokens.out
-
 cat frequency.txt | head -n 100 > top100.txt
 cat frequency.txt | tail -n 100 > bottom100.txt
+
+for file in $2/*
+do
+F=`echo "$file" | sed 's/.*\///g'`
+tr -sc 'A-Za-z0-9' '\n' < $file | tr A-Z a-z | sort | uniq -c | sort -n -r | head -n 100 > "top100$F"
+done
+
+rm *.java
+rm *.class
+rm -rf "./output"
+rm tokens.out
