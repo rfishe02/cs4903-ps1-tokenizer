9ed496ba114bfd27df649a5f1776d0c3211b317a
rfishe02 <43683461+rfishe02@users.noreply.github.com>
2019-06-15 00:16:26 -0500

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,5 +1,6 @@
+#!/bin/bash
 
-for file in $2/*
+for file in $1/*
 do
 F=`echo "$file" | sed 's/.*\///g'`
 tr -d ".'-" < $file | tr -sc 'A-Za-z0-9' '\n' | tr A-Z a-z | sort | uniq -c | sort -n -r | head -n 100 > "./results/top100$F"
