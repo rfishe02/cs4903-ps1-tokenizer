c9e9dfd7367d5913b761866c13c219aeeef20e5d
rfishe02 <43683461+rfishe02@users.noreply.github.com>
2019-06-15 14:22:31 -0500

    Update results.sh

--- results.sh
+++ results.sh
@@ -16,7 +16,7 @@ then
     #echo "creating freq.txt"
 
     #cat $1/*.txt > ./tokens.out
-    #tr -cd 'A-Za-z0-9\n' < tokens.out | tr A-Z a-z | sort | uniq -c | sort -T="./sort" -n -r > frequency.txt
+    #tr -cd 'A-Za-z0-9\n' < tokens.out | tr A-Z a-z | sort | uniq -c | sort -T "./sort" -n -r > frequency.txt
 
     if [ -d "./results" ]
     then
@@ -36,7 +36,7 @@ then
     for file in $1/*
     do
     F=`echo "$file" | sed 's/.*\///g'`
-    tr -d ".'-" < $file | tr -sc 'A-Za-z0-9' '\n' | tr A-Z a-z | sort -T=./sort | uniq -c | sort -n -r | head -n 100 > "./results/top100$F"
+    tr -d ".'-" < $file | tr -sc 'A-Za-z0-9' '\n' | tr A-Z a-z | sort -T=./sort | uniq -c | sort -T "./sort" -n -r | head -n 100 > "./results/top100$F"
     done
 
     #rm -rf "./output"
@@ -45,7 +45,7 @@ then
 fi
 
 #MISC
-#tr -d ".'-" < tokens.out | tr -sc 'A-Za-z0-9' '\n' | tr A-Z a-z | sort -T=./sort | uniq -c | sort -n -r > frequency.txt
-#tr -d ".'-" < $file | tr -sc 'A-Za-z0-9' '\n' | tr A-Z a-z | sort -T=./sort | uniq -c | sort -n -r | head -n 100 > "./results/top100$F"
+#tr -d ".'-" < tokens.out | tr -sc 'A-Za-z0-9' '\n' | tr A-Z a-z | sort -T=./sort | uniq -c | sort -T "./sort" -n -r > frequency.txt
+#tr -d ".'-" < $file | tr -sc 'A-Za-z0-9' '\n' | tr A-Z a-z | sort -T=./sort | uniq -c | sort -T "./sort" -n -r | head -n 100 > "./results/top100$F"
 
-#tr -cd 'A-Za-z0-9\n' < $file | tr A-Z a-z | sort | uniq -c | sort -T="./sort" -n -r | head -n 100 > "./results/top100$F"
+#tr -cd 'A-Za-z0-9\n' < $file | tr A-Z a-z | sort | uniq -c | sort -T "./sort" -n -r | head -n 100 > "./results/top100$F"
