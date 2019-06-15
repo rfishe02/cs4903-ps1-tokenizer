b3b78e7cca9adce7266283a9da0202191220378e
rfishe02 <43683461+rfishe02@users.noreply.github.com>
2019-06-15 12:18:52 -0500

    Update tokenizer.sh

--- tokenizer.sh
+++ tokenizer.sh
@@ -37,32 +37,3 @@ else
 
 fi
 
-#----------------------#
-
-cat $2/*.txt > ./tokens.out
-
-#tr -d ".'-" < tokens.out | tr -sc 'A-Za-z0-9' '\n' | tr A-Z a-z | sort | uniq -c | sort -n -r > frequency.txt
-tr -cd 'A-Za-z0-9\n@' < tokens.out | tr -sc 'A-Za-z0-9' '\n' | tr A-Z a-z | sort | uniq -c | sort -n -r > frequency.txt
-
-if [ -d "./results" ]
-then
-    rm -rf "./results"
-fi
-mkdir "./results"
-
-cat frequency.txt | head -n 100 > ./results/top100.txt
-cat frequency.txt | tail -n 100 > ./results/bottom100.txt
-
-rm tokens.out
-rm frequency.txt
-
-for file in $2/*
-do
-F=`echo "$file" | sed 's/.*\///g'`
-#tr -d ".'-" < $file | tr -sc 'A-Za-z0-9' '\n' | tr A-Z a-z | sort | uniq -c | sort -n -r | head -n 100 > "./results/top100$F"
-tr -cd 'A-Za-z0-9\n@' < $file | tr -sc 'A-Za-z0-9' '\n' | tr A-Z a-z | sort | uniq -c | sort -n -r | head -n 100 > "./results/top100$F"
-done
-
-rm *.java
-rm *.class
-#rm -rf "./output"
