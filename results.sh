583927eda911e7178ec548c45ae51c846a30bd21
rfishe02 <43683461+rfishe02@users.noreply.github.com>
2019-06-15 14:19:47 -0500

    Update results.sh

--- results.sh
+++ results.sh
@@ -1,43 +1,48 @@
 #!/bin/bash
 
-if [ -e "$1/other.txt" ] 
+if[ "${#@}" -gt "0" ]
 then
-    mv "$1/other.txt" "$1/other.out"
+
+    if [ -e "$1/other.txt" ] 
+    then
+        mv "$1/other.txt" "$1/other.out"
+    fi
+    if [ -d "./sort" ]
+    then
+        rm -rf "./sort"
+    fi
+    mkdir "./sort"
+
+    #echo "creating freq.txt"
+
+    #cat $1/*.txt > ./tokens.out
+    #tr -cd 'A-Za-z0-9\n' < tokens.out | tr A-Z a-z | sort | uniq -c | sort -T="./sort" -n -r > frequency.txt
+
+    if [ -d "./results" ]
+    then
+        rm -rf "./results"
+    fi
+    mkdir "./results"
+
+    #echo "creating top and bottom lists"
+
+    #cat frequency.txt | head -n 100 > ./results/top100.txt
+    #cat frequency.txt | tail -n 100 > ./results/bottom100.txt
+    #rm tokens.out
+    #rm frequency.txt
+
+    echo "creating top lists for each file"
+
+    for file in $1/*
+    do
+    F=`echo "$file" | sed 's/.*\///g'`
+    tr -d ".'-" < $file | tr -sc 'A-Za-z0-9' '\n' | tr A-Z a-z | sort -T=./sort | uniq -c | sort -n -r | head -n 100 > "./results/top100$F"
+    done
+
+    #rm -rf "./output"
+    #rm -rf "./sort"
+    
 fi
-if [ -d "./sort" ]
-then
-    rm -rf "./sort"
-fi
-mkdir "./sort"
-
-#echo "creating freq.txt"
-
-#cat $1/*.txt > ./tokens.out
-#tr -cd 'A-Za-z0-9\n' < tokens.out | tr A-Z a-z | sort | uniq -c | sort -T="./sort" -n -r > frequency.txt
-
-if [ -d "./results" ]
-then
-    rm -rf "./results"
-fi
-mkdir "./results"
-
-#echo "creating top and bottom lists"
-
-#cat frequency.txt | head -n 100 > ./results/top100.txt
-#cat frequency.txt | tail -n 100 > ./results/bottom100.txt
-#rm tokens.out
-#rm frequency.txt
-
-echo "creating top lists for each file"
-
-for file in $1/*
-do
-F=`echo "$file" | sed 's/.*\///g'`
-tr -d ".'-" < $file | tr -sc 'A-Za-z0-9' '\n' | tr A-Z a-z | sort -T=./sort | uniq -c | sort -n -r | head -n 100 > "./results/top100$F"
-done
-
-#rm -rf "./output"
-#rm -rf "./sort"
 
 #MISC
 #tr -d ".'-" < tokens.out | tr -sc 'A-Za-z0-9' '\n' | tr A-Z a-z | sort -T=./sort | uniq -c | sort -n -r > frequency.txt
