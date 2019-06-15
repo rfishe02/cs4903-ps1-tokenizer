3cc449c39c38fb5274648ee29ef330e9e194295f
rfishe02 <43683461+rfishe02@users.noreply.github.com>
2019-06-15 13:59:42 -0500

    Update results.sh

--- results.sh
+++ results.sh
@@ -4,8 +4,8 @@ mv "$1/other.txt" "$1/other.out"
 
 cat $1/*.txt > ./tokens.out
 
-#tr -d ".'-" < tokens.out | tr -sc 'A-Za-z0-9' '\n' | tr A-Z a-z | sort | uniq -c | sort -n -r > frequency.txt
-tr -cd 'A-Za-z0-9\n' < tokens.out | tr A-Z a-z | sort | uniq -c | sort -n -r > frequency.txt
+#tr -d ".'-" < tokens.out | tr -sc 'A-Za-z0-9' '\n' | tr A-Z a-z | sort -T=../sort | uniq -c | sort -n -r > frequency.txt
+tr -cd 'A-Za-z0-9\n' < tokens.out | tr A-Z a-z | sort | uniq -c | sort -T=../sort -n -r > frequency.txt
 
 if [ -d "./results" ]
 then
@@ -22,9 +22,8 @@ rm frequency.txt
 for file in $1/*
 do
 F=`echo "$file" | sed 's/.*\///g'`
-#tr -d ".'-" < $file | tr -sc 'A-Za-z0-9' '\n' | tr A-Z a-z | sort | uniq -c | sort -n -r | head -n 100 > "./results/top100$F"
-tr -cd 'A-Za-z0-9\n' < $file | tr A-Z a-z | sort | uniq -c | sort -n -r | head -n 100 > "./results/top100$F"
+#tr -d ".'-" < $file | tr -sc 'A-Za-z0-9' '\n' | tr A-Z a-z | sort -T=../sort | uniq -c | sort -n -r | head -n 100 > "./results/top100$F"
+tr -cd 'A-Za-z0-9\n' < $file | tr A-Z a-z | sort | uniq -c | sort -T=../sort -n -r | head -n 100 > "./results/top100$F"
 done
 
 #rm -rf "./output"
-
