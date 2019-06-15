7b7e545d0f788d71e57e8b4f099f6bc5eeccfec7
rfishe02 <43683461+rfishe02@users.noreply.github.com>
2019-06-15 15:00:25 -0500

    Update results.sh

--- results.sh
+++ results.sh
@@ -3,49 +3,56 @@
 if [ "${#@}" -gt "0" ]
 then
 
-    if [ -e "$1/other.txt" ] 
-    then
-        mv "$1/other.txt" "$1/other.out"
-    fi
-    if [ -d "./sort" ]
-    then
-        rm -rf "./sort"
-    fi
-    mkdir "./sort"
-
-    echo "creating freq.txt"
-
-    cat $1/*.txt > ./tokens.out
-    tr -cd 'A-Za-z0-9\n' < tokens.out | tr A-Z a-z | sort -T "./sort" | uniq -c | sort -T "./sort" -n -r > frequency.txt
-
+    # CREATE NECESSARY DIRECTORIES
+    
     if [ -d "./results" ]
     then
         rm -rf "./results"
     fi
+    
+    if [ -d "../sort" ]
+    then
+        rm -rf "./.sort"
+    fi
+   
     mkdir "./results"
+    mkdir "../sort"
+    
+    if [ -e "$1/other.txt" ] 
+    then
+        mv "$1/other.txt" "$1/other.out"
+    fi
+
+    # SORT & CREATE TOP & BOTTOM 100 LISTS, MINUS OTHER.TXT
+    
+    echo "combining .txt files"
+
+    cat $1/*.txt > ./tokens.out
+    tr -cd 'A-Za-z0-9\n' < tokens.out | tr A-Z a-z | sort -T "../sort" | uniq -c | sort -T "../sort" -n -r > frequency.txt
 
     echo "creating top and bottom lists"
 
     cat frequency.txt | head -n 100 > ./results/top100.txt
     cat frequency.txt | tail -n 100 > ./results/bottom100.txt
+    
     rm tokens.out
-    l/rm frequency.txt
+    rm frequency.txt
+
+    # SORT & CREATE TOP LISTS FOR EACH FILE
 
     echo "creating top lists for each file"
 
     for file in $1/*
     do
-    F=`echo "$file" | sed 's/.*\///g'`
-    tr -d ".'-" < $file | tr A-Z a-z | sort -T "./sort" | uniq -c | sort -T "./sort" -n -r | head -n 100 > "./results/top100$F"
+        F=`echo "$file" | sed 's/.*\///g'`
+        tr -cd 'A-Za-z0-9\n' < $file | tr A-Z a-z | sort -T "./sort" | uniq -c | sort -T "./sort" -n -r | head -n 100 > "./results/top100$F"
     done
 
+    rm -rf "../sort"
     #rm -rf "./output"
-    #rm -rf "./sort"
     
 fi
 
 #MISC
 #tr -d ".'-" < tokens.out | tr -sc 'A-Za-z0-9' '\n' | tr A-Z a-z | sort -T "./sort" | uniq -c | sort -T "./sort" -n -r > frequency.txt
 #tr -d ".'-" < $file | tr -sc 'A-Za-z0-9' '\n' | tr A-Z a-z | sort -T=./sort | uniq -c | sort -T "./sort" -n -r | head -n 100 > "./results/top100$F"
-
-#tr -cd 'A-Za-z0-9\n' < $file | tr A-Z a-z | sort -T "./sort" | uniq -c | sort -T "./sort" -n -r | head -n 100 > "./results/top100$F"
