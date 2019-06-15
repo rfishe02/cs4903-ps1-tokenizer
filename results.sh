5064aa4c95bcd5affc0c5d05dd46e355d05a1261
rfishe02 <43683461+rfishe02@users.noreply.github.com>
2019-06-15 14:13:35 -0500

    Update results.sh

--- results.sh
+++ results.sh
@@ -10,8 +10,10 @@ then
 fi
 mkdir "./sort"
 
+echo "creating freq.txt"
+
 cat $1/*.txt > ./tokens.out
-tr -cd 'A-Za-z0-9\n' < tokens.out | tr A-Z a-z | sort | uniq -c | sort -T=./sort -n -r > frequency.txt
+tr -cd 'A-Za-z0-9\n' < tokens.out | tr A-Z a-z | sort | uniq -c | sort -T="./sort" -n -r > frequency.txt
 
 if [ -d "./results" ]
 then
@@ -19,15 +21,19 @@ then
 fi
 mkdir "./results"
 
+echo "creating top and bottom lists"
+
 cat frequency.txt | head -n 100 > ./results/top100.txt
 cat frequency.txt | tail -n 100 > ./results/bottom100.txt
 rm tokens.out
 rm frequency.txt
 
+echo "creating top lists for each file"
+
 for file in $1/*
 do
 F=`echo "$file" | sed 's/.*\///g'`
-tr -cd 'A-Za-z0-9\n' < $file | tr A-Z a-z | sort | uniq -c | sort -T=./sort -n -r | head -n 100 > "./results/top100$F"
+tr -cd 'A-Za-z0-9\n' < $file | tr A-Z a-z | sort | uniq -c | sort -T="./sort" -n -r | head -n 100 > "./results/top100$F"
 done
 
 #rm -rf "./output"
