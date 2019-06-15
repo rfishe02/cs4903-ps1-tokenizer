66130b9bdbcd42861b51c16aafb0152845a02f4b
rfishe02 <43683461+rfishe02@users.noreply.github.com>
2019-06-15 00:12:31 -0500

    Update tokenizer.sh

--- tokenizer.sh
+++ tokenizer.sh
@@ -56,7 +56,7 @@ cat frequency.txt | tail -n 100 > ./results/bottom100.txt
 for file in $2/*
 do
 F=`echo "$file" | sed 's/.*\///g'`
-tr -sc 'A-Za-z0-9' '\n' < $file | tr A-Z a-z | sort | uniq -c | sort -n -r | head -n 100 > "./results/top100$F"
+tr -d ".'-" < $file | tr -sc 'A-Za-z0-9' '\n' | tr A-Z a-z | sort | uniq -c | sort -n -r | head -n 100 > "./results/top100$F"
 done
 
 rm *.java
