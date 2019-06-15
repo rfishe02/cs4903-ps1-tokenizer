85116b2da8632de69bf4c475be4d999fa8aa43c5
rfishe02 <43683461+rfishe02@users.noreply.github.com>
2019-06-15 12:20:35 -0500

    Update and rename test.sh to results.sh

--- results.sh
+++ results.sh
@@ -3,7 +3,7 @@
 cat $1/*.txt > ./tokens.out
 
 #tr -d ".'-" < tokens.out | tr -sc 'A-Za-z0-9' '\n' | tr A-Z a-z | sort | uniq -c | sort -n -r > frequency.txt
-tr -cd 'A-Za-z0-9\n' < tokens.out | tr A-Z a-z | sort | uniq -c | sort -n -r > frequency.txt
+tr -cd 'A-Za-z0-9\n@' < tokens.out | tr -sc 'A-Za-z0-9' '\n' | tr A-Z a-z | sort | uniq -c | sort -n -r > frequency.txt
 
 if [ -d "./results" ]
 then
@@ -21,6 +21,10 @@ for file in $1/*
 do
 F=`echo "$file" | sed 's/.*\///g'`
 #tr -d ".'-" < $file | tr -sc 'A-Za-z0-9' '\n' | tr A-Z a-z | sort | uniq -c | sort -n -r | head -n 100 > "./results/top100$F"
-tr -cd 'A-Za-z0-9\n' < $file | tr A-Z a-z | sort | uniq -c | sort -n -r | head -n 100 > "./results/top100$F"
+tr -cd 'A-Za-z0-9\n@' < $file | tr -sc 'A-Za-z0-9' '\n' | tr A-Z a-z | sort | uniq -c | sort -n -r | head -n 100 > "./results/top100$F"
 done
+
+rm *.java
+rm *.class
+#rm -rf "./output"
 
