b253c6443bbfe9f26d6c814ff1287e2bd5740aa0
rfishe02 <rfishe02@g.uafs.edu>
2019-06-24 13:33:47 -0500

    changes

--- results.sh
+++ results.sh
@@ -21,29 +21,23 @@ then
     mkdir $R
     mkdir $S
 
-    # SORT & CREATE TOP & BOTTOM 100 LISTS
+    # MERGE ALL OUT FILES INTO TXT
     
-    echo "combining output files"
+    echo "merging output files"
 
     cat $1/*.out > ./tokens.out
     tr -cd 'A-Za-z0-9\n@.-' < tokens.out | tr -sc 'A-Za-z0-9' '\n' | tr A-Z a-z | sort -T $S | uniq -c | sort -T $S -n -r > frequency.txt
-
-    echo "creating top and bottom lists for all"
-
-    cat frequency.txt | head -n 150 > ./results/top.txt
-    cat frequency.txt | tail -n 150 > ./results/bottom.txt
     
     rm tokens.out
-    rm frequency.txt
 
-    # SORT & CREATE TOP LISTS FOR EACH FILE
+    # CREATE LISTS FOR EACH FILE
 
-    echo "creating top lists for each file"
+    echo "condensing individual token files"
 
     for file in $1/*.txt
     do
         F=`echo "$file" | sed 's/.*\///g'`
-        tr -cd 'A-Za-z0-9\n.-' < $file | tr -sc 'A-Za-z0-9' '\n' | tr A-Z a-z | sort -T $S | uniq -c | sort -T $S -n -r | head -n 150 > "$R/top$F"
+        tr -cd 'A-Za-z0-9\n@.-' < $file | tr -sc 'A-Za-z0-9' '\n' | tr A-Z a-z | sort -T $S | uniq -c | sort -T $S -n -r > "$R/uniq_$F"
     done
 
     rm -rf $S
@@ -56,4 +50,8 @@ fi
 #tr -d ".'-" < tokens.out | tr -sc 'A-Za-z0-9' '\n' | tr A-Z a-z | sort -T "./sort" | uniq -c | sort -T "./sort" -n -r > frequency.txt
 #tr -d ".'-" < $file | tr -sc 'A-Za-z0-9' '\n' | tr A-Z a-z | sort -T=./sort | uniq -c | sort -T "./sort" -n -r | head -n 100 > "./results/top100$F"
 
-
+    # CREATE TOP & BOTTOM 100 LISTS
+    #echo "creating top and bottom lists for all"
+    #cat frequency.txt | head -n 150 > ./results/top.txt
+    #cat frequency.txt | tail -n 150 > ./results/bottom.txt
+    #rm frequency.txt
