fb5778c63c6d8e5fdba496e3835ec9d20b788a67
rfishe02 <rfishe02@g.uafs.edu>
2019-06-24 13:24:21 -0500

    changes to tokenizer script

--- tokenizer.sh
+++ tokenizer.sh
@@ -10,32 +10,54 @@ then
     rm -rf "./output"
 fi
 
-# TEST ON RANDOM FILES #
-
-if [ "${#@}" -gt "2" ]
+if [ -d "./in" ]
 then
+    rm -rf "./in"
+fi
+mkdir "./in"
 
-    if [ -d "./in" ]
+# TEST ON RANDOM SUBSET, RANDOM FILES, OR WHOLE DIRECTORY #
+# <INPUT DIR> <OUTPUT DIR> <NUM FILES> <REGEX>
+
+#SIZE=$( ls $1 | wc -l )
+
+if [ $3 -lt 10000 ] 
+then
+    
+    if [ "${#@}" -gt "3" ] 
+    then 
+
+        FILES=$( grep -lE $4 "$1/*" | shuf -n $3  )
+        
+        for f in $FILES
+        do
+            cp $f "./in"
+        done
+        
+        time java UATokenizer "./in" $2
+
+    elif [ "${#@}" -gt "2" ]
     then
-        rm -rf "./in"
+
+        NAMES=$( ls $1 | shuf -n $3 )
+
+        for n in $NAMES
+        do
+            F=$( find $1 -name $n)
+            cp $F "./in"
+        done
+
+        time java UATokenizer "./in" $2
+
+    else
+        time java UATokenizer $1 $2
     fi
-    mkdir "./in"
-
-    #/uafs/textmining/big
-    NAMES=$( ls $1 | shuf -n $3 )
-
-    for n in $NAMES
-    do
-        F=$( find $1 -name $n)
-        cp $F "./in"
-    done
-
-    time java UATokenizer "in" $2
 
 else
-    time java UATokenizer $1 $2
-
+    echo "TOO MANY FILES GIVEN"
 fi
+
+
 
 #rm *.java
 #rm *.class
